d29cebde9d5f52ed6cd65dd3835de88c19f4865e
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-04-20 15:59:16 +0300

    В файле orderByProps.js поправил возврат в map

--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,12 +1,10 @@
 export const orderByProps = (obj, sort) => {
-    const sortInArr = sort.map(key => {
-       return  {key: key, value: obj[key]};
-    })
+    const sortInArr = sort.map(key => ({key: key, value: obj[key]}))
 
     const sortObj = Object.keys(obj)
         .filter(item => !sort.includes(item))
         .sort((a, b) => a.localeCompare(b))
-        .map(key => {return {key: key, value: obj[key]}});
+        .map(key => ({key: key, value: obj[key]}));
 
     return [...sortInArr, ...sortObj] ;
 };
